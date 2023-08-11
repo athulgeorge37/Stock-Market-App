50e4c21601557383f0c1357b5047ca22023c63e7
athulgeorge37 <athulgeorge37@gmail.com>
2023-08-11 15:50:47 +1000

    adds dummy lorem text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,79 @@ export default function Home() {
                     href="/favicon.ico"
                 />
             </Head>
-            <main className="flex min-h-screen flex-col">Hello World</main>
+            <main className="flex min-h-screen flex-col">
+                Hello World
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Suscipit eveniet maxime perferendis, nihil unde ut
+                    laudantium, reiciendis corporis nam voluptatum esse
+                    consectetur excepturi sed labore fugiat ea eligendi eos
+                    quisquam corrupti ab. Aperiam non delectus adipisci sapiente
+                    exercitationem quos dolor labore, expedita, doloremque
+                    reiciendis impedit soluta repellendus ad ut aliquam, tempora
+                    ratione minus dicta fugit itaque cum! Numquam hic atque
+                    praesentium minus, neque distinctio. Eius iure consectetur
+                    aperiam minus corrupti nam magnam obcaecati doloribus non
+                    dignissimos numquam harum consequatur perferendis odio
+                    repudiandae corporis veritatis impedit, assumenda ipsa quos
+                    inventore eos iste? Exercitationem necessitatibus eius eum
+                    facere modi aliquid aspernatur tenetur amet! Vel
+                    necessitatibus dolore, consequatur dolores, nulla iste
+                    excepturi eos doloribus fugiat iure ea similique doloremque
+                    veritatis facere maiores cum recusandae delectus fugit? Unde
+                    inventore consequuntur labore, incidunt est quae odio rem
+                    delectus deserunt hic eum quas ratione quasi, voluptates
+                    blanditiis aliquid asperiores, nobis ex maxime tenetur.
+                    Molestias vel omnis laboriosam et, a perferendis saepe
+                    facilis itaque consequuntur soluta animi nobis ullam odit
+                    quia earum ea quaerat quis laborum maxime voluptatem. Quis
+                    quod temporibus excepturi nesciunt mollitia eum libero
+                    consectetur, dicta iusto a quas reiciendis dolor. Voluptas
+                    provident similique repellat dolores, nemo omnis, suscipit
+                    expedita, accusantium perferendis iusto officia in culpa sit
+                    pariatur recusandae esse. Eaque id temporibus beatae,
+                    consectetur inventore totam ab dignissimos qui deserunt,
+                    sapiente deleniti quasi dolorem magni enim praesentium?
+                    Beatae similique accusantium maiores! Eaque enim nostrum
+                    saepe nam nemo atque sequi a! Molestias inventore
+                    reprehenderit culpa sed impedit laudantium nemo? Aperiam eum
+                    ipsam nemo illum recusandae dolorum tempora tempore
+                    voluptates quibusdam repellat dignissimos, velit officiis ab
+                    unde fuga eos, voluptas odio impedit quidem ea. Magni beatae
+                    nemo non laborum earum fugiat odio adipisci quidem facilis
+                    reiciendis aperiam possimus, autem nostrum vitae sequi quam
+                    perferendis iusto blanditiis nisi incidunt assumenda omnis
+                    architecto debitis. Consectetur id sed quam soluta, maiores
+                    temporibus est illum nemo aspernatur mollitia,
+                    exercitationem repellendus harum, perspiciatis iste beatae
+                    doloremque quo ea minima? Accusantium sit in, beatae
+                    repudiandae praesentium culpa nulla saepe explicabo
+                    distinctio pariatur architecto aspernatur, reprehenderit
+                    officia ducimus nesciunt laborum, a libero ratione.
+                    Accusamus eum reprehenderit doloremque? Hic necessitatibus
+                    vel neque veritatis natus, commodi modi ipsa porro aliquam
+                    harum eos sint impedit nisi asperiores exercitationem
+                    tempora provident ullam nobis facere quo ea! Corporis,
+                    dolorem quidem quas ullam laudantium molestias eos nobis cum
+                    impedit temporibus ipsum consectetur consequatur asperiores
+                    magnam, odit dignissimos quia recusandae necessitatibus! Cum
+                    dicta aliquam totam porro? Accusamus molestias dolor quas
+                    illo accusantium dolore nam animi obcaecati, facere vel
+                    pariatur corrupti iste officiis unde quis dignissimos
+                    deleniti magnam laborum assumenda ad debitis, voluptas
+                    inventore consequatur tempora! Ad totam unde, molestiae
+                    veritatis maiores magni labore velit ex alias dicta
+                    voluptate facere esse culpa quod optio, voluptates qui quis
+                    consequatur dignissimos vitae similique officia sapiente
+                    neque. Commodi labore est optio quas sapiente suscipit
+                    laudantium, consectetur aliquam saepe quod reprehenderit
+                    eveniet eligendi ipsa quaerat, itaque unde consequatur
+                    cupiditate voluptate natus totam. Modi ut consectetur fugiat
+                    odit reiciendis quaerat sit. Molestiae iusto voluptates
+                    labore, reiciendis adipisci odio! Tempore corporis nemo
+                    deserunt et officia quis repudiandae.
+                </p>
+            </main>
         </>
     );
 }
